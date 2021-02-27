602f8c49a04ccf0dd7dff637df946f41f4535d06
victor-jvp <ing.vj15@gmail.com>
2021-02-27 00:51:44 -0400

    Tutorial 64 - React / React-Native - 32 - Adicionando íconos a los Tab

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Icon } from 'react-native-elements';
 
 import RestaurantsStack from './RestaurantsStack';
 import FavoritesStack from './FavoritesStack';
@@ -8,12 +9,52 @@ import TopRestaurantsStack from './TopRestaurantsStack';
 import SearchStack from './SearchStack';
 import AccountStack from './AccountStack';
 
+
 const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
+  const screenOptions = (route, color) => {
+    let iconName
+    switch (route.name) {
+      case "restaurants":
+        iconName = "compass-outline";
+        break;
+      case "favorites":
+        iconName = "heart-outline";
+        break;
+      case "top-restaurants":
+        iconName = "star-outline";
+        break;
+      case "search":
+        iconName = "magnify";
+        break;
+      case "account":
+        iconName = "home-outline";
+        break;
+    }
+
+    return (
+      <Icon
+        type="material-community"
+        name={iconName}
+        size={22}
+        color={color}
+      />
+    )
+  }
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          inactiveTintColor: "#a17dc3",
+          activeTintColor: "#442484",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="restaurants"
           component={RestaurantsStack}
@@ -41,5 +82,5 @@ export default function Navigation() {
         />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
