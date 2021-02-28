e04890f3a5e08f31332f75ae15dd11f9e5e67404
victor-jvp <ing.vj15@gmail.com>
2021-02-28 00:56:45 -0400

    Tutorial 64 - React / React-Native - 35 - Complentando la pantalla de usuario invitado

--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,14 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Loading from '../../components/Loading'
+import { StyleSheet, Text, ScrollView, Image } from 'react-native'
+import { Button } from 'react-native-elements'
 
 export default function UserGuest() {
   return (
-    <View>
-      <Text>UserGuest...</Text>
-      <Loading isVisible={true} text="Cargando..."/>
-    </View>
+    <ScrollView
+      centerContent
+      style={styles.viewBody}
+    >
+      <Image
+        source={require("../../assets/restaurant-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+      <Text style={styles.description}>¿Cómo describirías tu mejor restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla, vota cuál te ha gustado mas y comenta cómo ha sido tu experiencia</Text>
+      <Button
+        buttonStyle={styles.button}
+        title="Ver tu perfil"
+        onPress={() => console.log("Click!!!")}
+      />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewBody: {
+    marginHorizontal: 30
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    marginBottom: 10,
+    textAlign: "center"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginVertical: 10,
+    textAlign: "center"
+  },
+  description: {
+    textAlign: "justify",
+    marginBottom: 20,
+    color: "#a65273"
+  },
+  button: {
+    backgroundColor: "#442484"
+  }
+})
