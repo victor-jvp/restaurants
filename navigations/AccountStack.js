cc7b7dac38026f4547c7d4575543f3b0a74df853
victor-jvp <ing.vj15@gmail.com>
2021-02-27 22:29:13 -0400

    Revert "Tutorial 64 - React / React-Native - 33 - Identificando si el usuario está logueado o no"
    This reverts commit 2d3ee7d11bf8f42e8c4fb900922d9466c5326119.

--- navigations/AccountStack.js
+++ navigations/AccountStack.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import Account from '../screens/account/Account';
+import Account from '../screens/Account';
 
 const Stack = createStackNavigator()
 
--- screens/Account.js
+++ screens/Account.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function UserLogged() {
+export default function Account() {
   return (
     <View>
-      <Text>UserLogged...</Text>
+      <Text>Account</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({})
