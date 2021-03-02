798fd564445f3fa07202928703dddd6217376c42
Victor Veliz <ing.vj15@gmail.com>
2021-03-02 00:41:19 -0400

    Tutorial 64 - React / React-Native - 40 - Mostrar u ocultar la contraseña

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input } from 'react-native-elements'
+import { Button, Icon, Input } from 'react-native-elements'
 
 export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <View style={styles.form}>
-      <Input
-        containerStyle={styles.input}
-        placeholder="Ingresa tu email..."
-      />
+      <Input containerStyle={styles.input} placeholder="Ingresa tu email..." />
       <Input
         containerStyle={styles.input}
         placeholder="Ingresa tu contraseña..."
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={!showPassword}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.icon}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
       />
       <Input
         containerStyle={styles.input}
         placeholder="Confirma tu contraseña..."
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={!showPassword}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.icon}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
       />
       <Button
         title="Registrar Nuevo Usuario"
@@ -44,5 +59,8 @@ const styles = StyleSheet.create({
   },
   btn: {
     backgroundColor: "#442484"
+  },
+  icon: {
+    color: "#c1c1c1"
   }
 })
